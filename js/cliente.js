174b452767e426b2f02191062ac79bdba614cad1
LUIS FELIPE DE ALMEIDA NETTO <ART0060590@aluno.firjansenaisesi.com.br>
2023-09-11 11:59:42 -0300

    Colocando o cep para funcionar

--- js/cliente.js
+++ js/cliente.js
@@ -1,11 +1,45 @@
-const enderecoForm = document.querySelector(".form-body");
-const cepInput = document.querySelector("#cep");
-const ruaInput = document.querySelector("#rua");
-const bairroInput = document.querySelector("#bairro");
-const cidadeInput = document.querySelector("#cidade");
-const estadoInput = document.querySelector("#estado");
+var enderecoForm = document.querySelector(".form-body");
+var cepInput = document.querySelector("#cep");
+var ruaInput = document.querySelector("#rua");
+var bairroInput = document.querySelector("#bairro");
+var cidadeInput = document.querySelector("#cidade");
+var estadoInput = document.querySelector("#estado");
+var formInput = document.querySelector("[data-input]");
 
-cepInput.addEventListener("keypress", (e) => {
+cepInput.addEventListener("keyup", (e) =>{
+    const inputValue = e.target.value;
+
+    if (inputValue.length === 8){
+
+        getEndereco(inputValue);
+
+    }
+
+});
+
+var getEndereco = async (cep) =>{
+    const apiURL = `https://viacep.com.br/ws/${cep}/json/`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+    console.log(formInput);
+    console.log(data.erro);
+    ruaInput.value = data.logradouro;
+    cidadeInput.value = data.localidade;
+    bairroInput.value = data.bairro;
+    estadoInput.value = data.uf;
+}
+
+
+
+
+
+
+
+
+
+
+/*cepInput.addEventListener("keypress", (e) => {
     const onlyNumbers = /[0-9]/;
     const tecla = String.fromCharCode(e.keycode);
     if(!onlyNumbers.test(tecla)){
@@ -27,3 +61,4 @@ const getEndereco = async (cep) =>{
     const data = await response.json;
     Console.log(data);
 }
+))}*/
